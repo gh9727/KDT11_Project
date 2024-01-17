2db7520fba73cc2bb2195dc4abfd1d3f8e9781f7
gh9727 <gj9727@gmail.com>
2024-01-17 13:30:56 +0900

    project:0117

--- map.js
+++ map.js
@@ -2,13 +2,14 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
         center: new kakao.maps.LatLng(37.514575, 127.0495556), // 지도의 중심좌표
-        level: 8, // 지도의 확대 레벨
+        // 전역변수 : mapOption.level = 4; 이렇게 지도 레벨 변경 가능함
+        level: 10, // 지도의 확대 레벨
     };
-// 전역변수 : mapOption.level = 4; 이렇게 지도 레벨 변경 가능함
-// mapOption.level = 3;
-
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
+/* =======================================================================================================================================================
+ * ============================================레스토랑.js에 있는 식당 정보 positions 배열로 불러오기===============================================================
+ * ======================================================================================================================================================= */
 // MY : 위도 경도 입력을 위한 for문
 // MY : positions = [[음식점1, ...,위도1],[음식점2,..., 위도2],[음식점3,..., 위도3]]
 var positions = []; // MY : po
@@ -21,20 +22,20 @@ for (let i = 0; i < resturant.length; i++) {
         latlng: new kakao.maps.LatLng(resturant[i].coordinate[0], resturant[i].coordinate[1]), // 음식점 위치(위도,경도)
         src: resturant[i].src, // 식당 이미지 소스
         opening_hours: resturant[i].opening_hours, // 영업시간
-        closed_day: resturant[i].closed_day,
+        closed_day: resturant[i].closed_day, // 휴무일
         href: resturant[i].href, // 이 부분은 아직 넣지 않았음.
     });
 }
-// console.log(positions[1].name); -> 카카오 api로 인해 콘솔안찍힘
-
+/* =======================================================================================================================================================
+ * ============================================================마커, 커스텀 오버레이 생성을 위한 변수 선언============================================================================
+ * ======================================================================================================================================================= */
 // 마커 이미지의 이미지 주소입니다
-// var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
-
 var imageSrc = ['한식.png', '양식.png', '중식.png', '디저트.png', '일식.png', '분식.png'];
-let markers = []; // marker 전역변수로 변경 0116 11:20 수정 **************** search_koreanFood() 메서드 생성을 위해 수정 **********
-// marker
+let markers = []; // marker 정보를 담는 markers 전역변수(배열)로 선언 -> search_koreanFood() 메서드 작동이 안되서 변경함
+let overlays = []; // overlay 정보를 담는 overlays 전역변수(배열)로 선언 -> 위와 동일함
+
 // 마커 생성 + 마커 한식,중식,양식.. 별 맞는 이미지 넣기
-// 커스텀 오버레이 생성 + 오버레이에 들어갈 내용 createElement로 요소 생성
+/* ==============================================================마커,오버레이 생성을 위한 forEach문 시작======================================================================= */
 positions.forEach(function (pos) {
     // var imageSize = new kakao.maps.Size(24, 35);
     if (pos.category === '한식') {
@@ -56,16 +57,15 @@ positions.forEach(function (pos) {
         var imageSize = new kakao.maps.Size(24, 35);
         var markerImage = new kakao.maps.MarkerImage(imageSrc[5], imageSize);
     }
-    // var markerImage = new kakao.maps.MarkerImage(imageSrc[i], imageSize);
-    // } // 이쪽 제거하면됨
     // 마커를 생성합니다.
     let marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
         position: pos.latlng, // 마커를 표시할 위치
         image: markerImage, // 마커 이미지
     });
-    markers.push(marker); // *********** search_koreanFood() 메서드 생성을 위해 수정 *********
-    // 오버레이에 들어갈 정보를 생성합니다.
+    markers.push(marker); // 마커 정보를 markers 배열에 담기
+
+    // 커스텀 오버레이 생성 + 오버레이에 들어갈 내용 createElement로 요소 생성
     var content = document.createElement('div');
     content.className = 'wrap';
     var info = document.createElement('div');
@@ -93,10 +93,7 @@ positions.forEach(function (pos) {
     body.appendChild(img_div);
 
     var img = document.createElement('img');
-    // 이미지 배경을 각 음식점 사진으로 바꾸기 위해 pos.src
-    // img.src = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/thumnail.png';
     img.src = pos.src;
-    // 커스텀 오버레이에 들어가는 img 태그 기본값
     img.width = '73';
     img.height = '70';
     img_div.appendChild(img);
@@ -113,11 +110,9 @@ positions.forEach(function (pos) {
 
     var opening_hours = document.createElement('div');
     opening_hours.className = 'ellipsis jibun'; // 클래스명 address opening_hours 로 변경하기
-    // 이 부분 영업일이랑 영업 시간으로 채울거임
     opening_hours.appendChild(document.createTextNode(pos.opening_hours));
     desc.appendChild(opening_hours);
-    // 0115 1858 아직 작성중임 해야할 것 : opening_hours 요소 총 2개가 필요함 그거 레스토랑에 25개 데이터추가.. -> position에 레스토랑.오프닝아워 넣기 -> 여기 createElement 하기
-    // 0115 2033 종료
+
     var closed_day = document.createElement('div');
     closed_day.className = 'ellipsis jibun';
     closed_day.appendChild(document.createTextNode(pos.closed_day));
@@ -128,28 +123,46 @@ positions.forEach(function (pos) {
     var href = document.createElement('a');
 
     // 하이퍼링크 일단 푸딘코 사이트 글로 채웠음(0115 12:30)
-    // href.href = 'https://www.kakaocorp.com/main';
+    // href.href = 'https://www.kakaocorp.com/main'; -> 이 부분 블로그 사이트로 바꿔야함 아직 안함 0117 13:11 0117 13:11 0117 13:11 0117 13:11 0117 13:11
     href.href = `${pos.href}`;
     href.target = '_blank';
     href.className = 'link';
     href.appendChild(document.createTextNode('블로그'));
     href_div.appendChild(href);
+    // 커스텀 오버레이 변수
     var overlay = new kakao.maps.CustomOverlay({
         content: content,
         // map: map,
         position: marker.getPosition(),
     });
-    // 80%: foreach문을 for문이랑 합치면 된다.(0115 09:39 종료)
+    overlays.push(overlay); // overlays 배열에 overlay 정보 담기
+
     // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
     kakao.maps.event.addListener(marker, 'click', function () {
         overlay.setMap(map);
     });
 });
 // forEach문 종료
+/* =======================================================================================================================================================
+ * ====================================================================마커, 오버레이 자동 생성 메서드 ================================================================
+ * ======================================================================================================================================================= */
 
 // 모든 마커를 생성해주는 함수 -> 필터링 기능을 위해 제작함 0116
 function view_Marker() {
     for (let index = 0; index < positions.length; index++) {
         markers[index].setMap(map);
+    }
+}
+// 커스텀 오버레이 생성 -> 아직 사용할 곳 없어서 주석 0117 09:45
+// function view_overlay() {
+//     for (let index = 0; index < positions.length; index++) {
+//         overlays[index].setMap(map);
+//     }
+// }
+
+// 지도상 커스텀 오버레이 삭제 0117 09:30
+function delete_overlay() {
+    for (let index = 0; index < positions.length; index++) {
+        overlays[index].setMap(null);
     }
 }
--- search.js
+++ search.js
@@ -1,30 +1,20 @@
 // https://deveapp.com/map.php -> 지역 입력 시 위도 및 경도 반환해주는 사이트
-// <!-- 강남구 서초구 button 요소 클릭 시 구에 맞게 지도 중심좌표 이동 : 이 부분 onclick 이벤트 설정이 전부 안되서 질문 드림(0115 12:25) -->
-//                 <!-- click()이 문제였음 clk()로 변경해서 해결함 -->
-// 리더님 도움으로 에러(onclick 속성값을 click()으로 해서 생긴 오류였음)잡음(0115/12:49) + 이건 예시고 search.js 에 본 파일 옮겨야함(0115/ 13:27)
-// 0115 1730 종료
 
 // onclick 이벤트: 강남구 서초구 성동구 요소 클릭 시 지도 중심 좌표(map.center) 강남구청 서초역 성동소방서 로 변경 및 지도 레벨 변경(map.level)
 function search_Gangnam() {
-    // let level = map.getLevel();
     map.setLevel(6);
     let moveLatLng = new kakao.maps.LatLng(37.517331925853, 127.047377408384);
     map.setCenter(moveLatLng); // 강남구 요소 클릭 시 지도 중심 좌표를 강남구청으로 변경
-    // map.setCenter(37.517331925853, 127.047377408384);
-    // mapOption.center = new kakao.maps.LatLng(37.517331925853, 127.047377408384);
-    // mapOption.level = 4;
 }
 function search_Seocho() {
     map.setLevel(6);
     let moveLatLng = new kakao.maps.LatLng(37.49197968304073, 127.00759880987736);
     map.setCenter(moveLatLng); // 서초구 요소 클릭 시 지도 중심 좌표를 서초구청으로 변경 -> 서초구청 이동 시 너무 떨어져있어 서초역 위도로 변경
-    // mapOption.center = new kakao.maps.LatLng(37.483588810333, 127.032734543897);
 }
 function search_Seongdong() {
     map.setLevel(6);
     let moveLatLng = new kakao.maps.LatLng(37.55385421924312, 127.0393834897108);
     map.setCenter(moveLatLng); // 성동구 요소 클릭 시 지도 중심 좌표를 성동구청으로 변경 -> 여기도 식당과 떨어져있어 성동소방서 위도로 변경
-    // mapOption.center = new kakao.maps.LatLng(37.563427205337, 127.036930141185);
 }
 
 // 필터링 : 한식 중식 양식 일식 분식 카페
@@ -34,6 +24,7 @@ function search_Seongdong() {
 // 한식 리스트 클릭 시
 function search_koreanFood() {
     view_Marker();
+    delete_overlay();
     let category = '한식'; // this.value
     for (let i = 0; i < positions.length; i++) {
         if (category !== positions[i].category) {
@@ -44,6 +35,7 @@ function search_koreanFood() {
 // 중식 리스트 클릭 시
 function search_chineseFood() {
     view_Marker();
+    delete_overlay();
     let category = '중식'; // this.value
     for (let i = 0; i < positions.length; i++) {
         if (category !== positions[i].category) {
@@ -54,6 +46,7 @@ function search_chineseFood() {
 // 양식 리스트 클릭 시
 function search_westernFood() {
     view_Marker();
+    delete_overlay();
     let category = '양식'; // this.value
     for (let i = 0; i < positions.length; i++) {
         if (category !== positions[i].category) {
@@ -64,6 +57,7 @@ function search_westernFood() {
 // 일식 리스트 클릭 시
 function search_japaneseFood() {
     view_Marker();
+    delete_overlay();
     let category = '일식'; // this.value
     for (let i = 0; i < positions.length; i++) {
         if (category !== positions[i].category) {
@@ -74,8 +68,10 @@ function search_japaneseFood() {
 // 분식 리스트 클릭 시
 function search_snackBar() {
     view_Marker();
+    delete_overlay();
     let category = '분식'; // this.value
     for (let i = 0; i < positions.length; i++) {
+        delete_overlay();
         if (category !== positions[i].category) {
             markers[i].setMap(null);
         }
@@ -84,31 +80,21 @@ function search_snackBar() {
 // 카페 리스트 클릭 시
 function search_cafe() {
     view_Marker();
+    delete_overlay();
     let category = '디저트'; // this.value
     for (let i = 0; i < positions.length; i++) {
+        delete_overlay();
         if (category !== positions[i].category) {
             markers[i].setMap(null);
         }
     }
 }
 
-// 필터링 작업 0116 12:11 종료
-
-// 사용자 입력창 이벤트 0116 이 부분 아직 못함 -> 0116 21:23 마무리
-// let district_input = document.querySelector('#district').value;
-// let food_input = document.querySelector('#food').value;
-// console.log(district_input);
-// console.log(food_input);
-// if (district_input.includes('강남' || '강남구')) {
-//     map.setLevel(6);
-//     let moveLatLng = new kakao.maps.LatLng(37.517331925853, 127.047377408384);
-//     map.setCenter(moveLatLng); // 강남구 요소 클릭 시 지도 중심 좌표를 강남구청으로 변경
-// }
-
 // 0116 21:00 moveLatlng 이 변수 전역변수(배열)로 선언하는게 더 깔끔할듯 내일 가능하면 수정하기
-let district_button = document.querySelector('.district_button');
+
 // 지역(구) 사용자 입력폼 창 내부 버튼에 클릭 이벤트 적용
 // ex) 사용자가 "강남(구)" 입력 후 입력폼 창 내부의 버튼 클릭: 지도 중심좌표가 강남구로 이동
+let district_button = document.querySelector('.district_button');
 district_button.onclick = function () {
     let district_input = document.querySelector('#district').value;
     if (district_input.includes('강남' || '강남구')) {
@@ -126,14 +112,15 @@ district_button.onclick = function () {
     }
 };
 
-let food_button = document.querySelector('.food_button'); // 버튼클래스 요소를 받아옴
 // 종류(한식,중식 등..) 사용자 입력폼 창 내부 버튼에 클릭 이벤트 적용
 //  ex) 사용자가 "한식" 입력 후 버튼 클릭: 한식 마커만 지도상에 출력
+let food_button = document.querySelector('.food_button'); // 버튼클래스 요소를 받아옴
 food_button.onclick = function () {
     let food_input = document.querySelector('#food').value;
     let category = ['한식', '중식', '양식', '일식', '분식', '디저트'];
     if (food_input.includes(category[0])) {
         view_Marker();
+        delete_overlay();
         for (let i = 0; i < positions.length; i++) {
             if (category[0] !== positions[i].category) {
                 markers[i].setMap(null);
@@ -141,6 +128,7 @@ food_button.onclick = function () {
         }
     } else if (food_input.includes(category[1])) {
         view_Marker();
+        delete_overlay();
         for (let i = 0; i < positions.length; i++) {
             if (category[1] !== positions[i].category) {
                 markers[i].setMap(null);
@@ -148,6 +136,7 @@ food_button.onclick = function () {
         }
     } else if (food_input.includes(category[2])) {
         view_Marker();
+        delete_overlay();
         for (let i = 0; i < positions.length; i++) {
             if (category[2] !== positions[i].category) {
                 markers[i].setMap(null);
@@ -155,6 +144,7 @@ food_button.onclick = function () {
         }
     } else if (food_input.includes(category[3])) {
         view_Marker();
+        delete_overlay();
         for (let i = 0; i < positions.length; i++) {
             if (category[3] !== positions[i].category) {
                 markers[i].setMap(null);
@@ -162,6 +152,7 @@ food_button.onclick = function () {
         }
     } else if (food_input.includes(category[4])) {
         view_Marker();
+        delete_overlay();
         for (let i = 0; i < positions.length; i++) {
             if (category[4] !== positions[i].category) {
                 markers[i].setMap(null);
@@ -169,11 +160,25 @@ food_button.onclick = function () {
         }
     } else if (food_input.includes(category[5])) {
         view_Marker();
+        delete_overlay();
         for (let i = 0; i < positions.length; i++) {
             if (category[5] !== positions[i].category) {
                 markers[i].setMap(null);
             }
         }
     }
+    // 한식,중식 입력외에 음식점 이름 입력 시 해당 음식점 마커만 남기고 전부
+    // 나머지 음식점 마커 전부 삭제 + 해당 음식점 오버레이 출력 0117 09:45 종료
+    else {
+        view_Marker();
+        delete_overlay();
+        for (let i = 0; i < positions.length; i++) {
+            if (food_input !== positions[i].name) {
+                markers[i].setMap(null);
+            } else {
+                map.setCenter(new kakao.maps.LatLng(resturant[i].coordinate[0], resturant[i].coordinate[1])); //-> 이 부분 제대로 작동안함
+                overlays[i].setMap(map);
+            }
+        }
+    }
 };
-// 0116 21:17 사용자 입력폼 이벤트 추가 종료
